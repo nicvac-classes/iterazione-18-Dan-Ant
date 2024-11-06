a1d5cb642cfa04d904bfc839ec8516a3b767e4fd
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2024-11-06 10:56:15 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,36 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+float a, tot, med;
+int g, i;
+g=7;
+tot=0;
+i=0;
+while(i<g)
+{
+    cout << "Qual è il livello dell'acqua oggi?" << endl;
+    cin >> a;
+    if(a>10)
+    {
+        cout << "Il valore dell'acqua è stato superato di 10cm il giorno " << i << endl;
+        
+    }
+    else
+    {
+
+    }
+    tot=tot+a;
+    i=i+1;
+}
+med=tot/g;
+if(med>0)
+{
+    cout<<"Acqua alta" << endl;
+}
+else
+{
+    cout << "Livello normale" << endl;
+}
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
